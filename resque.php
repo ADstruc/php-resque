c8bc2abfc414d048663ae671bfb2a60a33264dfa
Andrew Shults <andrewjshults@gmail.com>
2012-02-28 10:08:31 -0500

    Remove the multiple worker code and now the workers output what they are listening to when they start

--- resque.php
+++ resque.php
@@ -39,42 +39,15 @@ if(!empty($INTERVAL)) {
 	$interval = $INTERVAL;
 }
 
-$count = 1;
-$COUNT = getenv('COUNT');
-if(!empty($COUNT) && $COUNT > 1) {
-	$count = $COUNT;
-}
+$queues = explode(',', $QUEUE);
+$worker = new Resque_Worker($queues);
+$worker->logLevel = $logLevel;
 
-if($count > 1) {
-	for($i = 0; $i < $count; ++$i) {
-		$pid = pcntl_fork();
-		if($pid == -1) {
-			die("Could not fork worker ".$i."\n");
-		}
-		// Child, start the worker
-		else if(!$pid) {
-			$queues = explode(',', $QUEUE);
-			$worker = new Resque_Worker($queues);
-			$worker->logLevel = $logLevel;
-			fwrite(STDOUT, '*** Starting worker '.$worker."\n");
-			$worker->work($interval);
-			break;
-		}
-	}
+$PIDFILE = getenv('PIDFILE');
+if ($PIDFILE) {
+    file_put_contents($PIDFILE, getmypid()) or
+        die('Could not write PID information to ' . $PIDFILE);
 }
-// Start a single worker
-else {
-	$queues = explode(',', $QUEUE);
-	$worker = new Resque_Worker($queues);
-	$worker->logLevel = $logLevel;
-	
-	$PIDFILE = getenv('PIDFILE');
-	if ($PIDFILE) {
-		file_put_contents($PIDFILE, getmypid()) or
-			die('Could not write PID information to ' . $PIDFILE);
-	}
-
-	fwrite(STDOUT, '*** Starting worker '.$worker."\n");
-	$worker->work($interval);
-}
-?>
+fwrite(STDOUT, "*** Listening to $REDIS_BACKEND/$REDIS_DATABASE\n");
+fwrite(STDOUT, "*** Starting worker $worker\n");
+$worker->work($interval);
